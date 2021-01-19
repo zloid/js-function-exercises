4a9fc6b4dd661626bb903b05ef56760c15ed34ce
zloid <zloid-1@yandex.by>
2021-01-19 14:31:17 +0100

    add(function): flatListOfItems

--- index.test.js
+++ index.test.js
@@ -1,29 +1,29 @@
-"use strict"
+'use strict'
 
 // 1. Please write a function that reverses a string
 function reverseAString(string = '') {
-  return string.split("").reverse().join("")
+    return string.split('').reverse().join('')
 }
 
 // 2. Please write a function that filters out numbers from a list
 function filtersOutNumbers(list = []) {
-  if (list.length === 0) {
-    return list
-  }
-  const excludeInfinity = list.filter((element) => element !== Infinity)
-  const excludeNaN = excludeInfinity.filter(
-    (element) => isNaN(element) !== true
-  )
-  return excludeNaN.filter((element) => typeof element === "number")
+    if (list.length === 0) {
+        return list
+    }
+    const excludeInfinity = list.filter((element) => element !== Infinity)
+    const excludeNaN = excludeInfinity.filter(
+        (element) => isNaN(element) !== true
+    )
+    return excludeNaN.filter((element) => typeof element === 'number')
 }
 
 // 3. Please write a function that shows the usage of closures
 function usageOfClosures() {
-  let counter = 0
-  return () => {
-    counter += 1
-    console.log(counter)
-  }
+    let counter = 0
+    return () => {
+        counter += 1
+        console.log(counter)
+    }
 }
 let localScopeOne = usageOfClosures()
 let localScopeTwo = usageOfClosures()
@@ -34,13 +34,23 @@ localScopeTwo() //1
 // 4. Please write a recursive function that flattens an list of items
 // example input [[2, [4, [44,5,6]]], [4,5,6], [[2,4], 4], 5]
 // example output [2, 4, 44, 5, 6, 4, 5, 6, 2, 4, 4, 5]
-function flattensAnListOfItems(list = []) {
-  return list.flat()
+function flatListOfItems(list = [], outputArray = []) {
+    if (list.length === 0) return outputArray
+
+    let firstElement = list.splice(0, 1)[0]
+
+    if (Array.isArray(firstElement)) {
+        return flatListOfItems([...firstElement, ...list], outputArray)
+    } else {
+        outputArray.push(firstElement)
+        return flatListOfItems(list, outputArray)
+    }
 }
 
 // 5. Please write a function that finds all common elements of two arrays(only primitive types as array elements, order doesn't matter)
 // example inputs ['b', 3, 4, 76, 'c'], ['a', 'b', 4, 76, 21, 'e']
 // example output ['b', 4, 76]
+function findAllCommonElements(arrayOne = [], arrayTwo = []) {}
 
 // 6. Please write a function that finds all different elements of two arrays(only primitive types as array elements, order doesn't matter)
 // example inputs ['b', 3, 4, 76, 'c'], ['a', 'b', 4, 76, 21, 'e']
@@ -71,31 +81,45 @@ function flattensAnListOfItems(list = []) {
 // example output { id: '22' }
 
 // Jest tests
-describe("functions from index.js", () => {
-  it("1) reverseAString", () => {
-    expect(reverseAString("DOM")).toBe("MOD")
-    expect(reverseAString("Metallica")).toBe("acillateM")
-  })
+describe('functions from index.js', () => {
+    it('1) reverseAString', () => {
+        expect(reverseAString('DOM')).toBe('MOD')
+        expect(reverseAString('Metallica')).toBe('acillateM')
+    })
 
-  it("2) filtersOutNumbers", () => {
-    expect(filtersOutNumbers([])).toEqual([])
-    expect(
-      filtersOutNumbers([
-        Infinity,
-        1,
-        2,
-        "a",
-        3.3,
-        "b",
-        null,
-        undefined,
-        NaN,
-        { a: 3 },
-      ])
-    ).toEqual([1, 2, 3.3])
-  })
+    it('2) filtersOutNumbers', () => {
+        expect(filtersOutNumbers([])).toEqual([])
+        expect(
+            filtersOutNumbers([
+                Infinity,
+                1,
+                2,
+                'a',
+                3.3,
+                'b',
+                null,
+                undefined,
+                NaN,
+                { a: 3 },
+            ])
+        ).toEqual([1, 2, 3.3])
+    })
 
-  it("4) flattensAnListOfItems", () => {
-    expect(flattensAnListOfItems([[2, [4, [44,5,6]]], [4,5,6], [[2,4], 4], 5])).toEqual([2, 4, 44, 5, 6, 2, 4, 4, 5])
-  })
+    it('4) flatListOfItems', () => {
+        expect(
+            flatListOfItems([[2, [4, [44, 5, 6]]], [4, 5, 6], [[2, 4], 4], 5])
+        ).toEqual([2, 4, 44, 5, 6, 4, 5, 6, 2, 4, 4, 5])
+        expect(flatListOfItems([1, 2, 3, [4, 5, [6, 7]], 8])).toEqual([
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+        ])
+    })
 })
+
+it('5) findAllCommonElements', () => {})
